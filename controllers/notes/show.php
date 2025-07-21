5c5732049717b73f113e2286cb80df9f794da42a
riaanjlagrange <riaanjlagrange@gmail.com>
2025-07-21 17:11:46 +0200

    add a delete button to make a delete request to the database WIP

--- controllers/notes/show.php
+++ controllers/notes/show.php
@@ -7,15 +7,31 @@ $username = 'root';
 $password = 'P@ssw0rd';
 $db = new Database($config['database'], $username, $password);
 
-$userId = 3;
+$userId = 8;
 
 $id = $_GET['id'];
 
-$note = $db->query("SELECT * FROM notes WHERE id = :id", ["id" => $id])->fetchOrAbort();
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    // show the note first
+    $note = $db->query("SELECT * FROM notes WHERE id = :id", ["id" => $id])->fetchOrAbort();
 
-authorize($note['user_id'] === $userId);
+    // check if the user is authorized to delete the note
+    authorize($note['user_id'] === $userId);
 
-view("notes/show.view.php", [
-    "heading" => "View note",
-    "note" => $note,
-]);
+    // delete the note form the database
+    $db->query("DELETE FROM notes WHERE id = :id", [
+	"id" => $id,
+    ]);
+
+    header("location: /notes");
+    exit();
+} else {
+    $note = $db->query("SELECT * FROM notes WHERE id = :id", ["id" => $id])->fetchOrAbort();
+
+    authorize($note['user_id'] === $userId);
+
+    view("notes/show.view.php", [
+	"heading" => "View note",
+	"note" => $note,
+    ]);
+}
--- views/notes/show.view.php
+++ views/notes/show.view.php
@@ -6,8 +6,12 @@
 
 	  <main>
 	    <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-		  <a href="/notes" class="text-white bg-blue-500 bold rounded-sm px-5 py-3 hover:bg-blue-600">Back to all notes</a>
-		  <p class="mt-5"><?= htmlspecialchars($note['body']); ?></p>
+	      <a href="/notes" class="text-white bg-blue-500 bold rounded-sm px-5 py-3 hover:bg-blue-600">Back to all notes</a>
+	      <p class="mt-5"><?= htmlspecialchars($note['body']); ?></p>
+	      <form class="mt-5" method="POST">
+		  <input type="hidden" name="id" value="<?= $note['id']; ?>" />
+		  <button type="submit" class="text-red-500 hover:underline">Delete</button>
+	      </form>
 	    </div>
 	  </main>
 
